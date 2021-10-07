1736d68803a9c993ba0ea573f12862a553551f62
govind6498 <govindsingh6498@gmail.com>
2021-10-07 21:05:08 +0530

    Added:Ability to check daily Employee Wage

---
+++ TotalEmployeeWage.js
@@ -0,0 +1,21 @@
+//USE CASE 2
+const IS_PART_TIME =1;
+const IS_FULL_TIME =2;
+const PART_TIME_HOURS =4;
+const FULL_TIME_HOURS =8;
+const Wage_PER_HOURS = 20;
+let empHrs=0;
+let empCheck = Math.floor(Math.random()*10)%3;
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
+        break;
+}
+let empWage = empHrs*Wage_PER_HOURS;
+console.log("Emp Wage:"+empWage)
